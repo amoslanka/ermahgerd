0afe1290caf5e694075d9b8772403ecc2b826a75
amoslanka <amoslanka@gmail.com>
2012-07-24 21:10:04 -0700

    HERP DERP!

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 gem 'sinatra'
 gem 'json'
+gem 'haml'
 
 group :development do
   gem 'shotgun'
--- ermahgerd.rb
+++ ermahgerd.rb
@@ -1,22 +1,27 @@
-# require 'CGI'
-
 class Ermahgerd < Sinatra::Base
 
   get "/" do
-    "Hello Bundler"
+    haml :index
   end
 
-  get "/translate/:string" do
-    # content_type :json
-    # translate(params[:string])
-    ::CGI::unescape translate(params[:string])
-    # "hi"
-    # params[:string]
+  post "/ternslert" do 
+    translate_hash params
   end
-
+  get  "/ternslert" do
+    translate_hash params
+  end
   post "/translate" do
+    redirect "/ternslert"
+  end
+  get  "/translate" do
+    redirect "/ternslert"
+  end
+
+  private 
+
+  def translate_hash input_hash
     hash = {}
-    params.each_pair do |key, value|
+    input_hash.each_pair do |key, value|
       if value.is_a?(String)
         hash[key] = translate(value)
       end
@@ -24,28 +29,27 @@ class Ermahgerd < Sinatra::Base
     hash.to_json
   end
 
-end
+  def translate text
+    text.upcase!
+    text.gsub!(/[AEIOUY]r(?! )/i, 'E');
+    text.gsub!(/AA/i, 'A');
+    text.gsub!(/EE/i, 'E');
+    text.gsub!(/II/i, 'I');
+    text.gsub!(/OO/i, 'O');
+    text.gsub!(/UU/i, 'U');
+    text.gsub!(/YY/i, 'Y');
+    text.gsub!(/[AEIOUY]{2,}/i, 'E');
+    text.gsub!(/[AEIOUY](?! )/i, 'ER');
+    text.gsub!(/[Y]/i, 'ER');
+    text.gsub!(/ERH/i, 'ER');
+    text.gsub!(/ERR/i, 'ER');
+    text.gsub!(/MER/i, 'MAH');
+    text.gsub!('ERWERSERMAH', 'ERSUM');
+    text.gsub!('ERWERSERME', 'ERSUM');
+    text.gsub!('GERSERBERMPS', 'GERSBERMS');
+    text.gsub!('MAHMAH', 'MERM');
+    text.gsub!('MAHME', 'MERM');
+    text
+  end
 
-def translate text
-
-  text.upcase!
-  text.gsub!(/[AEIOUY]r(?! )/i, 'E');
-  text.gsub!(/AA/i, 'A');
-  text.gsub!(/EE/i, 'E');
-  text.gsub!(/II/i, 'I');
-  text.gsub!(/OO/i, 'O');
-  text.gsub!(/UU/i, 'U');
-  text.gsub!(/YY/i, 'Y');
-  text.gsub!(/[AEIOUY]{2,}/i, 'E');
-  text.gsub!(/[AEIOUY](?! )/i, 'ER');
-  text.gsub!(/[Y]/i, 'ER');
-  text.gsub!(/ERH/i, 'ER');
-  text.gsub!(/ERR/i, 'ER');
-  text.gsub!(/MER/i, 'MAH');
-  text.gsub!('ERWERSERMAH', 'ERSUM');
-  text.gsub!('ERWERSERME', 'ERSUM');
-  text.gsub!('GERSERBERMPS', 'GERSBERMS');
-  text.gsub!('MAHMAH', 'MERM');
-  text.gsub!('MAHME', 'MERM');
-  text
 end
